b000e92dbaf8805658d334bfb6ab8d383938628d
YesCoops <coopandmgc@gmail.com>
2022-03-31 07:57:02 +0000

    Initial commit

---
+++ evens.py
@@ -0,0 +1,5 @@
+def even_number_of_evens(numbers):
+    return None
+
+if __name__ == '__main__':
+    print(even_number_of_evens(5))
---
+++ test_evens.py
@@ -0,0 +1,8 @@
+import unittest
+from evens import even_number_of_evens
+
+class TestEvents(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
