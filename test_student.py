5145ae05087c7132517f0483660e536c2bef0c16
YesCoops <coopandmgc@gmail.com>
2022-04-04 11:34:30 +0000

    Refactored tests

--- test_student.py
+++ test_student.py
@@ -4,21 +4,26 @@ from student import Student
 
 class TestStudent(unittest.TestCase):
 
-    def test_full_name(self):
-        student = Student('John', 'Doe')
+    def setUp(self):
+        print('setUp')
+        self.student = Student('John', 'Doe')
 
-        self.assertEqual(student.full_name, 'John Doe')
+    def tearDown(self):
+        print('tearDown')
+
+    def test_full_name(self):
+        print('test_full_name')
+        self.assertEqual(self.student.full_name, 'John Doe')
 
     def test_alert_santa(self):
-        student = Student('John', 'Doe')
-        student.alert_santa()
+        print('test_alert_santa')
+        self.student.alert_santa()
 
-        self.assertTrue((student.naughty_list))
+        self.assertTrue(self.student.naughty_list)
 
     def test_email(self):
-        student = Student('John', 'Doe')
-
-        self.assertEqual(student.email, 'john.doe@email.com')
+        print('test_email')
+        self.assertEqual(self.student.email, 'john.doe@email.com')
 
 
 
