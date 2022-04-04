6441d8a6aecd09c05a4c53800b8f9b79ed086afe
YesCoops <coopandmgc@gmail.com>
2022-04-04 13:10:24 +0000

    Set mocking up for database retrieval

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 class Student:
     """A student class as base for method testing"""
@@ -9,16 +10,26 @@ class Student:
         self._start_date = date.today()
         self.end_date = date.today() + timedelta(days=365)
         self.naughty_list = False
-
     
     @property
     def full_name(self):
         return f"{self._first_name} {self._last_name}"
 
-    def alert_santa(self):
-        self.naughty_list = True
-
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days) 
+
+    def course_schedule(self):
+        response = requests.get(f"http://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
         
--- test_student.py
+++ test_student.py
@@ -1,12 +1,22 @@
 import unittest
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
+
     def setUp(self):
         print('setUp')
         self.student = Student('John', 'Doe')
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tear down Class")
 
     def tearDown(self):
         print('tearDown')
@@ -25,7 +35,28 @@ class TestStudent(unittest.TestCase):
         print('test_email')
         self.assertEqual(self.student.email, 'john.doe@email.com')
 
+    def test_apply_extension(self):
+        print('test_apply_extension')
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
 
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5))
+
+    def test_course_schedule_success(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Success")
+
+    def test_course_schedule_failed(self):
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+
+            schedule = self.student.course_schedule()
+            self.assertEqual(schedule, "Something went wrong with the request!")
+      
 
 if __name__ == "__main__":
     unittest.main()
